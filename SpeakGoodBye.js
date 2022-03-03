a980308ae6bfb025dddc075036742708bd9d3716
Sachindra1234 <90864604+Sachindra1234@users.noreply.github.com>
2022-03-04 01:59:16 +0530

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,11 @@
+// My Solution //
+
+(function(window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = function (name) {
+  		console.log(speakWord + " " + name);
+	}
+	
+	window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ SpeakHello.js
@@ -0,0 +1,11 @@
+// My Solution //
+
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
